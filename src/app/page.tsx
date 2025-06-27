5166cc9efac9c8d0d8e018c0443e28b0feae5547
amiparadis250 <pishimweaime7@gmail.com>
2025-06-27 20:25:52 +0200

    will set up our healthpulse landing page next js boiler plate

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import next from "next";
+
+export default async function Page() {
+  return(
+    <div>
+      <h1 className="text-center top-100 ">This is  landing page for  healthpulse</h1>
+    </div>
+  )
+
+}
