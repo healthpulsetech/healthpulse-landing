004e06033eb5242b5e006550afefc56dcf6e4d85
amiparadis250 <pishimweaime7@gmail.com>
2025-07-07 08:15:36 +0200

    will add fix hero section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -29,7 +29,7 @@ const ContactUs = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+    <div className=" bg-background flex items-center justify-center p-4">
       <div className="w-full max-w-4xl bg-white rounded-xl shadow-lg overflow-hidden">
         <div className="flex flex-col lg:flex-row">
           {/* Left side - Form */}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ import rafiki from "@/assets/rafiki.jpg"
 
 const Hero: React.FC = () => {
   return (
-    <div className=" flex flex-col lg:flex-row items-center justify-center pt-0 px-4 ">
+    <div className=" flex flex-col lg:flex-row items-center justify-center pt-0 px-4 font-sans ">
       {/* Text Content */}
       <motion.div
         className="w-full lg:w-1/2 flex justify-center items-center p-4 lg:p-8 order-2 lg:order-1"
