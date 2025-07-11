2cd4a4b1d1629d571048ebf71a9d4fee8f1efc98
amiparadis250 <pishimweaime7@gmail.com>
2025-07-11 14:03:20 +0200

    will add partners page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { FlatCompat } from "@eslint/eslintrc"; // Not needed if disabled
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // All configs disabled
 ];
 
 export default eslintConfig;
